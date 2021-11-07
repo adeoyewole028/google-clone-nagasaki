9f6d5823e7ea090caa05ab215a1f4c77735e0c8e
MickelsonKlaus <84320904+MickelsonKlaus@users.noreply.github.com>
2021-11-07 17:53:34 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Home from './Pages/Home';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+		<div className="App">
+			<Switch>
+				<Route path="/" exact component={Home} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
